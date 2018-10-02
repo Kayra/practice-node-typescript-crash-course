f312948a2ed94d3b0790eeb42fbbdc6f55ab98a2
kayra <kayraalat@gmail.com>
2018-10-02 11:34:27 +0100

    Configured the application to user body parser middleware

--- lib/app.ts
+++ lib/app.ts
@@ -1,11 +1,20 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
+
 
 class App {
 
   public app: express.Application;
 
+  private config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+
   constructor() {
     this.app = express();
+    this.config();
   }
 
 }
